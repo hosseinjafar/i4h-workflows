c7bf93898eaba637a27faef9f811b7264b1e259b
hosseinjafar <hosseinj.2555@gmail.com>
2026-01-05 18:09:21 -0500

    made the docker comose for i4h-brachyutils. next is to build the dockerfile

--- workflows/brachytherapy/docker_src/command_docker.sh
+++ workflows/brachytherapy/docker_src/command_docker.sh
@@ -6,15 +6,15 @@ export GID=$(id -g)
 export DISPLAY=$DISPLAY
 # export HOST_HOME=${HOME}
 # # to build the image and run the container
-# export DOCKER_BUILDKIT=1
-# echo $(ssh-agent)
-# ssh-add ~/.ssh/id_rsa # to allow the container to access the host SSH keys
-# docker compose up --build -d BrachyUtils
+export DOCKER_BUILDKIT=1
+echo $(ssh-agent)
+ssh-add ~/.ssh/id_rsa # to allow the container to access the host SSH keys
+docker compose up --build -d I4H-BrachyUtils
 # # to run the container without building the image
-docker compose up --no-build -d BrachyUtils
+# docker compose up --no-build -d BrachyUtils
 # docker compose up --no-build -d DoseCalcMC
 # docker compose up --no-build -d DoseCalcTG43
 # docker compose up --no-build -d Plastimatch
 # docker compose up --no-build -d SimpleElastix
 # to enter the container
-docker exec -it BrachyUtils bash
+docker exec -it I4H-BrachyUtils bash
