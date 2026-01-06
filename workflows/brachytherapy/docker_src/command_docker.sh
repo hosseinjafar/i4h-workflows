d3fddd1b5177661cdcbc4a4af77c5b8c8659ff00
hosseinjafar <hosseinj.2555@gmail.com>
2026-01-06 11:38:57 -0500

    since we have nvidia-smi working on my docker container. imma move on with installing i4h inside my container

--- workflows/brachytherapy/docker_src/command_docker.sh
+++ workflows/brachytherapy/docker_src/command_docker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # xhost +local: # to allow the container to access the host display
 # set the environment variables for the container
-export UID=$(id -u)
-export GID=$(id -g)
-export DISPLAY=$DISPLAY
+# export UID=$(id -u)
+# export GID=$(id -g)
+# export DISPLAY=$DISPLAY
 # export HOST_HOME=${HOME}
 # # to build the image and run the container
-export DOCKER_BUILDKIT=1
-echo $(ssh-agent)
-ssh-add ~/.ssh/id_rsa # to allow the container to access the host SSH keys
+# export DOCKER_BUILDKIT=1
+# echo $(ssh-agent)
+# ssh-add ~/.ssh/id_rsa # to allow the container to access the host SSH keys
 docker compose up --build -d I4H-BrachyUtils
 # # to run the container without building the image
 # docker compose up --no-build -d BrachyUtils
